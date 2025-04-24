36010d2608573d0aa165dc2c19144d471bc9f1a9
dizatal <talaspayevad@gmail.com>
2025-04-24 09:25:19 +0200

    Update buggy_python_code.py

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -62,7 +62,5 @@ if __name__ == '__main__':
         load_yaml(input("File name: "))
         print("Executed -ls on current folder")
     elif choice == "4":
-    password = input("Enter master password: ")
-    authenticate(password)
-
-
+        password = input("Enter master password: ")
+        authenticate(password)
